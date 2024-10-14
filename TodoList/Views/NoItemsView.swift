046ceec36d7c3b410e3e99ba7b8421272b262d62
jackie <ss611611@gmail.com>
2024-10-14 17:45:19 +0800

    User Experience and Animations

---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,61 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Jackie Lu on 2024/10/14.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? I think you should click the add button and add a bunch of items to you todo list!")
+                    .padding(.bottom, 20)
+                NavigationLink(destination: AddView()) {
+                    Text("Add Something 🥳")
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? secondaryAccentColor : Color.accentColor, in: .rect(cornerRadius: 10))
+                }
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7), radius: animate ? 30 : 10, y: animate ? 50 : 30)
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        NoItemsView()
+            .navigationTitle("Title")
+    }
+}
